0bc73c2158572f3f29346eeef0dda9946ca06c31
tsucchinoko <ccya2211@icloud.com>
2025-08-14 10:30:04 +0900

    doc: ブロックコメントを使わないよう修正

--- problem/merge-string-alternately/go/main.go
+++ problem/merge-string-alternately/go/main.go
@@ -17,11 +17,9 @@ func mergeAlternately(word1 string, word2 string) string {
 	return string(res)
 }
 
-/**
- * - 内部でバッファを管理しており、繰り返しの連結で何度もメモリを確保し直すことを抑制できる。
- * - `[]byte` で自分でメモリ管理をするよりもシンプルに書ける。
- * - 大量の文字列を繰り返し連結する場合のパフォーマンスが良い
- */
+// - 内部でバッファを管理しており、繰り返しの連結で何度もメモリを確保し直すことを抑制できる。
+// - `[]byte` で自分でメモリ管理をするよりもシンプルに書ける。
+// - 大量の文字列を繰り返し連結する場合のパフォーマンスが良い
 func mergeAlternately2(word1, word2 string) string {
 	var b strings.Builder
 	b.Grow(len(word1) + len(word2)) // 十分な容量を確保
