d45caefe211e9059a207fe1f65a127a2767bd886
tsucchinoko <ccya2211@icloud.com>
2025-11-09 16:13:35 +0900

    feat: 母音の判定を関数化

--- problem/reverse-vowels-of-a-string/go/main.go
+++ problem/reverse-vowels-of-a-string/go/main.go
@@ -4,24 +4,31 @@ import (
 	"fmt"
 )
 
+func isVowel(c byte) bool {
+	switch c {
+	case 'a', 'e', 'i', 'o', 'u':
+		return true
+	case 'A', 'E', 'I', 'O', 'U':
+		return true
+	default:
+		return false
+	}
+}
+
 func reverseVowels(s string) string {
 	if len(s) <= 1 {
 		return s
-	}
-	vowels := map[byte]bool{
-		'a': true, 'e': true, 'i': true, 'o': true, 'u': true,
-		'A': true, 'E': true, 'I': true, 'O': true, 'U': true,
 	}
 	b := []byte(s)
 	left, right := 0, len(s)-1
 
 	for left < right {
 		// 左を母音に移動
-		for left < right && !vowels[b[left]] {
+		for left < right && !isVowel(b[left]) {
 			left++
 		}
 		// 右を母音に移動
-		for left < right && !vowels[b[right]] {
+		for left < right && !isVowel(b[right]) {
 			right--
 		}
 		// 左右の母音を交換
